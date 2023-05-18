4a3592025921742e1dfdf107992cf96e60717e10
alheston <43995264+alheston@users.noreply.github.com>
2023-05-18 19:43:59 +0000

    adding model directory

---
+++ model/customers.sql
@@ -0,0 +1,13 @@
+SELECT 
+	orders.customer_id AS customer_id,
+	customers.name AS name,
+	customers.email AS email,
+	MIN(orders.created_at) AS first_order_at,
+	COUNT(orders.created_at) AS nr_of_orders
+FROM `analytics-engineers-club.coffee_shop.customers` AS customers
+INNER JOIN `analytics-engineers-club.coffee_shop.orders` AS orders ON customers.id=orders.customer_id
+GROUP BY 
+	customer_id,
+	name,
+	email
+ORDER BY first_order_at limit 5;
