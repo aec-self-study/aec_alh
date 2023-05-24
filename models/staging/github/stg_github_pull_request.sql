cb8f14b777d6e030ef51bafbfac873355145ebaa
alheston <43995264+alheston@users.noreply.github.com>
2023-05-24 18:35:58 +0000

    adding untracked files

---
+++ models/staging/github/stg_github_pull_request.sql
@@ -0,0 +1,32 @@
+{{ config(materialized='table') }}
+
+with source as (
+
+    select * from {{ source('github', 'pull_request') }}
+
+),
+
+renamed as (
+
+    select
+        id as pull_request_id,
+        _fivetran_synced,
+        base_label,
+        base_ref,
+        base_repo_id,
+        base_sha,
+        base_user_id,
+        draft as is_draft,
+        head_label,
+        head_ref,
+        head_repo_id,
+        head_sha,
+        head_user_id,
+        issue_id,
+        merge_commit_sha
+
+    from source
+
+)
+
+select * from renamed
