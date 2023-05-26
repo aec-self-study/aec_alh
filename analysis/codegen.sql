9037c1c6f931d6e4d41f55de7b2dc6bbf8254969
alheston <43995264+alheston@users.noreply.github.com>
2023-05-26 14:25:34 +0000

    adding changes

--- analysis/codegen.sql
+++ analysis/codegen.sql
@@ -1,17 +1,23 @@
-{{ codegen.generate_base_model(
-    source_name='github',
-    table_name='issue',
-    materialized='table'
-) }}
+-- {{ codegen.generate_base_model(
+--     source_name='github',
+--     table_name='issue',
+--     materialized='table'
+-- ) }}
+
+-- {{ codegen.generate_base_model(
+--     source_name='github',
+--     table_name='pull_request',
+--     materialized='table'
+-- ) }}
+
+-- {{ codegen.generate_base_model(
+--     source_name='github',
+--     table_name='repository',
+--     materialized='table'
+-- ) }}
 
 {{ codegen.generate_base_model(
-    source_name='github',
-    table_name='pull_request',
-    materialized='table'
-) }}
-
-{{ codegen.generate_base_model(
-    source_name='github',
-    table_name='repository',
+    source_name='web_tracking',
+    table_name='pageviews',
     materialized='table'
 ) }}
