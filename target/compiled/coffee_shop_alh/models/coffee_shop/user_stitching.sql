fd315849b6b81ff41bd80b758af8e5f132c9d313
alheston <43995264+alheston@users.noreply.github.com>
2023-05-27 14:18:20 +0000

    adding user_session_sql file

---
+++ target/compiled/coffee_shop_alh/models/coffee_shop/user_stitching.sql
@@ -0,0 +1,20 @@
+with source as (
+
+    select * from `analytics-engineers-club`.`web_tracking`.`pageviews`
+
+),
+
+SELECT
+  id,
+  customer_id,
+  first_value(visitor_id) OVER (PARTITION BY customer_id ORDER BY timestamp ASC) as vistor_id, 
+  device_type,
+  timestamp,
+  page
+FROM
+  source
+WHERE
+  customer_id IS NOT NULL
+)
+
+select * from renamed
